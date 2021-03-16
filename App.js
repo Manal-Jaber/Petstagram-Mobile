16d8af591f2c334384d18f64b894c0a1dd04a3b8
Manal-Jaber <manal.jaber94@gmail.com>
2021-03-16 04:22:49 +0200

    #1F- Developed Login Page

--- App.js
+++ App.js
@@ -1,8 +1,41 @@
-import React from 'react';
-import Login from './components/login/Login'
+import React, {useEffect} from 'react';
+import Login from './components/login/Login';
 
 export default function App() {
+// console.log(process.env.APP_MANIFEST.extra.Host)
   return (
     <Login/>
   );
 }
+// import React, { useState, useEffect } from 'react';
+
+// import { Text, View, StyleSheet } from 'react-native';
+// import { AppLoading } from 'expo';
+// import { useFonts, Cookie_400Regular } from '@expo-google-fonts/cookie';
+
+// export default () => {
+//   let [fontsLoaded] = useFonts({
+//     Cookie_400Regular,
+//   });
+
+//   let fontSize = 24;
+//   let paddingVertical = 6;
+
+//   if (!fontsLoaded) {
+//     return <AppLoading/>;
+//   } else {
+//     return (
+//       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+//         <Text
+//           style={{
+//             fontSize,
+//             paddingVertical,
+//             // Note the quoting of the value for `fontFamily` here; it expects a string!
+//             fontFamily: 'Cookie_400Regular',
+//           }}>
+//           Cookie Regular
+//         </Text>
+//       </View>
+//     );
+//   }
+// };
--- components/login/Login.js
+++ components/login/Login.js
@@ -1,42 +1,129 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Image, Text, View, TextInput, TouchableOpacity } from 'react-native';
+import Svg, { Line} from 'react-native-svg';
 
 import { AppLoading } from 'expo';
-import {
-    useFonts,
-    Cookie_400Regular,
-  } from '@expo-google-fonts/dev';
+import { Cookie_400Regular } from '@expo-google-fonts/cookie';
+import { Alice_400Regular } from '@expo-google-fonts/alice';
+import { useFonts } from 'expo-font';
+
+import logo from '../../assets/logo.png';
+import facebook from '../../assets/facebook.png';
+import google from '../../assets/google.png';
 
 export default function Login() {
-    let [fontsLoaded] = useFonts({
-        Cookie_400Regular,
-      });
-    
-    if (!fontsLoaded) {
-    // return <AppLoading />;
-    return <Text>App Loading</Text>;
+  let [fontsLoaded] = useFonts({
+    Cookie_400Regular,
+    Alice_400Regular
+  });
 
-    } else {
+  // if (!fontsLoaded) {
+  //   return <AppLoading/>;
+  // } else {
     return (
-        <View style={styles.container}>
-        <Text style={styles.logo}>Petstagram</Text>
-        <StatusBar style="auto" />
+      <View style={styles.container}>
+        <Svg height="1000" width="10" style={{position:'absolute', zIndex:-1, alignSelf:'flex-start'}}>
+          <Line x1="10" y1="0" x2="10" y2="1000" stroke="#FEC3B9" strokeWidth="5" />
+        </Svg>
+        <View style={styles.logo}>
+          <Image source={logo} style={{height:100, width:100}}/>
+          <Text style={styles.logoText}>Petstagram</Text>
         </View>
+        <Svg height="10" width="500">
+          <Line x1="0" y1="0" x2="500" y2="0" stroke="#FEC3B9" strokeWidth="5" />
+        </Svg>
+        <TextInput placeholder='Username' style={styles.text}/>
+        <TextInput placeholder='Password' style={styles.text}/>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Login</Text>
+        </TouchableOpacity>
+        <View>
+          <View style={styles.loginContainer}>
+            <Image source={facebook} style={{height:50, width:50}}/>
+            <TouchableOpacity>
+              <Text style={styles.loginText}>Login via Facebook</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.loginContainer}>
+            <Image source={google} style={{height:50, width:50}}/>
+            <TouchableOpacity>
+              <Text style={styles.loginText}>Login via Google</Text>
+            </TouchableOpacity>
+          </View>
+          <Text style={styles.or}>or {"\n"}.</Text>
+          <TouchableOpacity>
+            <Text style={styles.loginText}>Create a new account</Text>
+          </TouchableOpacity>
+        </View>
+        <StatusBar style="auto"/>
+      </View>
     );
     }
-}
-
+// }
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    flexDirection:'column',
     alignItems: 'center',
-    // justifyContent: 'center',
+    justifyContent: 'space-evenly',
+    margin:20
   },
   logo: {
-      fontFamily: 'Cookie_400Regular',
-      fontSize: 50,
-      fontStyle: 'italic'
+    flex: 1,
+    flexDirection:'row',
+    justifyContent:'center',
+    justifyContent: 'space-between',
+    maxHeight:100,
+    marginTop: 20,
+  },
+  logoText: {
+    fontSize:50,
+    paddingVertical:6,
+    fontFamily: 'Cookie_400Regular',
+    alignSelf: 'center',
+    height: 70
+  },
+  text: {
+    fontSize:14,
+    paddingVertical:6,
+    fontFamily: 'Alice_400Regular',
+    width:200,
+    textAlign:'center',
+    borderColor: '#624224',
+    borderWidth: 1,
+    marginTop: 20
+  },
+   button: {
+     backgroundColor: '#EBD4BD',
+     borderColor: '#624224',
+     borderWidth: 1,
+     width: 80,
+     marginTop: 20
+   },
+   buttonText: {
+     fontSize:24,
+     paddingVertical:6,
+     fontFamily: 'Cookie_400Regular',
+     textAlign: 'center'
+  },
+  loginContainer: {
+    width: 200,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+    marginTop:10
+  },
+  loginText: {
+    fontSize: 14,
+    paddingVertical:6,
+    fontFamily: 'Alice_400Regular',
+    textAlign: 'center'
+  },
+  or: {
+    fontSize: 30,
+    paddingVertical:6,
+    fontFamily: 'Cookie_400Regular',
+    textAlign: 'center'
   }
 });
