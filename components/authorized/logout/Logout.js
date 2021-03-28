cd7496013348e688f4b4a278a00d26e453047f07
Manal-Jaber <manal.jaber94@gmail.com>
2021-03-28 05:50:33 +0300

    #6F- Finished Edit Profile and Add Profile

--- components/authorized/logout/Logout.js
+++ components/authorized/logout/Logout.js
@@ -26,6 +26,6 @@ const styles = StyleSheet.create({
       fontSize:15,
       paddingVertical:6,
       fontFamily: 'Salsa_400Regular',
-      color: 'red'
+      color: '#750404'
     },
   });
--- components/authorized/profile/AddProfile.js
+++ components/authorized/profile/AddProfile.js
@@ -1,13 +1,63 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState, useEffect } from 'react';
-import { ScrollView, StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
+import { ScrollView, StyleSheet, Text, View, TouchableOpacity, Image, TextInput } from 'react-native';
+import {Picker} from '@react-native-picker/picker';
+import { useNavigation } from '@react-navigation/native';
 
-export default function AddProfile(props) {
+import ProfilePicker from './ProfilePicker';
 
+import profile from '../../../assets/profile.jpg'
+
+export default function EditProfile(props) {
+
+    const categories = [
+        {
+            id : 0,
+            name : 'Cat',
+        },
+        {
+            id : 1,
+            name : 'Dog'
+        }
+    ]
+    const [category, setCategory] = useState(categories[0]);
   return (
     <ScrollView>
       <View style={styles.container}>
-        <Text>Add Profile</Text>
+        <View style={{alignSelf:'flex-end'}}>
+            <ProfilePicker/>
+        </View>
+        <View style={{alignItems:'center'}}>
+            <Image source={profile} style={styles.bigProfile}/>
+            <Text style={styles.ProfileText}>Add Profile Pic</Text>
+        </View>
+        <View style={{margin:10}}>
+            <Text style={styles.ProfileInfo}>Username</Text>
+            <TextInput style={styles.ProfileInput} placeholder='Username'/>
+        </View>
+        <View style={{margin:10}}>
+            <Text style={styles.ProfileInfo}>Category</Text>
+            <Picker
+                selectedValue={category.id}
+                onValueChange={(e) => setCategory(e)}
+                >
+                <Picker.Item label={categories[0].name} value={categories[0].id} />
+                <Picker.Item label={categories[1].name} value={categories[1].id} />
+                {/* {categories.map((cat)=>{
+                    <Picker.Item key={cat.id} label={cat.name} value={cat.id} />
+                    {console.log(cat.name)}
+                })} */}
+            </Picker>
+        </View>
+        <View style={{margin:10}}>
+            <Text style={styles.ProfileInfo}>Bio</Text>
+            <TextInput style={styles.BioInput} placeholder='Bio'/>
+        </View>
+        <View style={{flex: 1, flexDirection: 'row', justifyContent: 'center'}}>
+          <TouchableOpacity style={styles.Button} onPress={()=>navigation.navigate('ViewProfile')}>
+            <Text style={[styles.ProfileInfo, {color:'rgba(0,0,0,0.8)', textAlign: 'center',}]}>Add</Text>
+          </TouchableOpacity>
+        </View>
         <StatusBar style="auto"/>
       </View>
     </ScrollView>
@@ -19,5 +69,43 @@ const styles = StyleSheet.create({
     flexDirection:'column',
     justifyContent: 'flex-start',
     marginTop: 10,
+  },
+  bigProfile: {
+    height:70,
+    width:70,
+    borderRadius: 70
+  },
+  ProfileText: {
+    fontSize:15,
+    paddingVertical:6,
+    fontFamily: 'Salsa_400Regular',
+  },
+  ProfileInfo: {
+    fontSize:30,
+    paddingVertical:6,
+    fontFamily: 'Cookie_400Regular',
+    color: 'rgba(0,0,0,0.7)'
+  },
+  ProfileInput: {
+    fontSize:20,
+    paddingVertical:6,
+    fontFamily: 'Salsa_400Regular',
+    borderBottomColor:'black',
+    borderBottomWidth: 1,
+  },
+  BioInput: {
+    fontFamily: 'Roboto',
+    fontSize: 12,
+    color: 'rgba(0,0,0,0.7)',
+    borderBottomColor:'black',
+    borderBottomWidth: 1,
+  },
+  Button: {
+    borderColor: 'black',
+    borderWidth: 1,
+    backgroundColor: '#EBD4BD',
+    padding: 5,
+    width: 150,
+    alignSelf: 'center'
   },
 });
